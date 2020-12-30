8022626b15f8f9acc49b2e19382f7b54993269ee
Raj Malhotra <//>
2020-12-30 14:29:42 +0100

    Initial Commit

---
+++ Drawing3/Drawing3App.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing3App.swift
+//  Drawing3
+//
+//  Created by Raj Malhotra on 30.12.20.
+//
+
+import SwiftUI
+
+@main
+struct Drawing3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
